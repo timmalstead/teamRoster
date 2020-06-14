fa9675d3c29a615efd2dd27308fac99407caac46
Tim Malstead <timmmalstead@gmail.com>
2020-06-14 12:51:54 -0700

    reduced data

--- index.js
+++ index.js
@@ -14,7 +14,32 @@ express.get("/", async (request, response) => {
     includeGridData: true,
   })
 
-  response.json(rosterSheet)
+  const rowData = rosterSheet.data.sheets[0].data[0].rowData
+  const teamData = rowData.reduce((arr, data, i) => {
+    const dV = data.values
+    const fV = "formattedValue"
+    const n = "None"
+    if (i > 0 && dV[8][fV] === "Active")
+      arr.push({
+        fullName: dV[0][fV] || n,
+        firstName: dV[1][fV] || n,
+        lastName: dV[2][fV] || n,
+        role: dV[3][fV] || n,
+        email: dV[4][fV] || n,
+        gitHub: dV[6][fV] || n,
+        picture: dV[7][fV] || n,
+      })
+    return arr
+  }, [])
+
+  response.json({
+    status: {
+      code: rosterSheet.status,
+      statusText: rosterSheet.statusText,
+    },
+    message: "Roster Sucessfully Retrieved from Google Sheets",
+    data: teamData,
+  })
 })
 
 express.listen(p.PORT, () =>
