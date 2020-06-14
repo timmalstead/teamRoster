69cc61051907f85b6de05a083bea1d606988c5ea
Tim Malstead <timmmalstead@gmail.com>
2020-06-13 22:44:01 -0700

    backup before data sorting

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const { google } = require("googleapis")
 const express = require("express")()
 require("dotenv").config()
 const p = process.env
 
-express.get("/", (request, response) => {
-  response.send("hello")
+express.get("/", async (request, response) => {
+  const auth = new google.auth.OAuth2(p.CLIENT_ID, p.CLIENT_SECRET)
+  auth.setCredentials({ refresh_token: p.REFRESH_TOKEN })
+
+  const sheets = google.sheets({ version: "v4", auth })
+
+  const rosterSheet = await sheets.spreadsheets.get({
+    spreadsheetId: p.SPREADSHEET_ID,
+    includeGridData: true,
+  })
+
+  response.json(rosterSheet)
 })
 
 express.listen(p.PORT, () =>
